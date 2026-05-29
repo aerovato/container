3e4aee3a6bb01a8505e6eb56df18857bd5ad1d23
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2026-05-29 11:58:58 -0400

    feat: Improve DI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,28 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: ["dist/", "node_modules/", "__mocks__/", "scripts/*.js"],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist/", "node_modules/", "__mocks__/", "scripts/*.js"],
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "fs",
+              message: 'Importing "fs" is only allowed in main.ts',
+            },
+            {
+              name: "child_process",
+              message: 'Importing "child_process" is only allowed in main.ts',
+            },
+          ],
+        },
+      ],
+    },
   },
 );
