2c2b3388530ac1c26e8607655316b579c46129f4
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2026-05-30 13:36:48 -0400

    feat: Rebrand

--- src/tos.ts
+++ src/tos.ts
@@ -4,29 +4,26 @@ import { SettingsStore } from "./config";
 export const LATEST_TOS_VERSION = 3;
 
 const TOS = `
-\x1b[33mWarning: Security Advisory\x1b[0m
+\x1b[33mSecurity Advisory\x1b[0m
 
-The main purpose of container is to protect commands like 'rm' or 'apt'
-from unintentionally affecting your main system.
+The purpose of 'container' is to protect commands like 'rm' or 'apt' from
+unintentionally affecting your main system.
 
-container does not protect from prompt injections in the event that an agent
-becomes malaligned.
-
-This is an innate problem within coding harness software and container does
-not attempt to solve it.
+container does not protect against LLM-related security issues such as prompt
+injections.
 
 Users are advised to not download or work with unverified software.
 - Sensitive information inside the container may still be exfiltrated by
   an attacker just as with your regular system.
-  - This includes:
+- This includes:
   - OAuth credentials inside harness configs
   - API keys inside harness configs
   - SSH keys for git functionality if enabled
 
 Never install or run your harness on unverified software. By using container,
-you agree that you are aware of these risks and will not hold the author
-liable for any outcomes arising from usage of the software.
-`;
+you agree that you are aware of these risks and will not hold the author liable
+for any outcomes arising from usage of the software.
+`.trim();
 
 export async function ensureTosAccepted(
   settingsStore: SettingsStore,
